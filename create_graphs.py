5bea07c434b78de8335d361bfc86adbe145ea188
Peter Bachman <peter.j.bachman@gmail.com>
2022-08-31 11:58:52 -0500

    update graph

--- create_graphs.py
+++ create_graphs.py
@@ -27,19 +27,20 @@ plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.5)
 plt.savefig("plots/sentimentPlot.pdf")
 
 # Create Histograms
-fig, ax = plt.subplots(nrows=2, sharex=True)
+fig, ax = plt.subplots(ncols=2, sharey=True)
 n_bins = 20
 
 # Show Afinn Plot
 ax[0].hist(afinnSimFiltered['totalValue'], bins=n_bins)
 ax[0].axvline(0, linestyle='--', color="gray")
 ax[0].set_title("Afinn Sentiment Dictionary")
+ax[0].set_xlabel("Average Similarity to Positive and Negative Sentiment")
+ax[0].xaxis.set_label_coords(1, -.1)
 
 # Show Bing Plot
 ax[1].hist(bingSimFiltered['totalValue'], bins=n_bins)
 ax[1].axvline(0, linestyle='--', color="gray")
 ax[1].set_title("Bing Sentiment Dictionary")
-ax[1].set_xlabel("Average Similarity to Positive and Negative Sentiment")
 
 plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.5)
 plt.savefig("plots/sentimentHist.pdf")
